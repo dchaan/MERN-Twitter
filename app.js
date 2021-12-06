bbc1ac8fad723fbc023197fde8e4baeae356850a
David Chan <dchaan1@gmail.com>
2021-12-05 17:40:09 -0800

    Create first tweet

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {res.send("Hello a/A!")});
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {console.log('Listening on port ${port}')})
